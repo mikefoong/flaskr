0dbb55bf89c201a0a7b09a7310cb0bfaabe94e0e
mikefoong <mikefoong@yahoo.com>
2017-04-20 21:07:12 +0800

    Adding flaskr project

---
+++ flaskr.py
@@ -0,0 +1,46 @@
+#all the imports
+import os
+import sqlite3
+from flask import Flask, request, session, g, redirect, url_for, abort, render_template, flash
+
+app = Flask(__name__) # create the application instance :)
+app.config.from_object(__name__) #Load config from this file, flaskr.py
+
+#load default config and override config from an environment variable
+app.config.update(dict(
+    DATABASE=os.path.join(app.root_path, 'flaskr.db'),
+    SECRET_KEY='development key',
+    USERNAME='admin',
+    PASSWORD='default'
+))
+app.config.from_envvar('FLASKR_SETTINGS', silent=True)
+
+def get_db():
+    """ Opens a new database connection if there is none yet for the current application context"""
+    if not hasattr(g, 'sqlite_db'):
+        g.sqlite_db = connect_db()
+    return g.sqlite_db
+
+@app.teardown_appcontext
+def close_db(error):
+    """Closes the database at the end of the request"""
+    if hasattr(g, 'sqlite_db'):
+        g.sqlite_db.close()
+
+def connect_db():
+    """Connects to the specific datbase."""
+    rv = sqlite3.connect(app.config['DATABASE'])
+    rv.row_factory = sqlite3.Row
+    return rv
+
+def init_db():
+    db = get_db()
+    with app.open_resource('schema.sql', mode='r') as f:
+        db.cursor().executescript(f.read())
+    db.commit()
+
+@app.cli.command('initdb')
+def initdb_command():
+    """Initialises the database"""
+    init_db()
+    print('Initialised the database.')
