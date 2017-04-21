c4baa8338d07c3210287339272f7410a04a5747f
mikefoong <mikefoong@yahoo.com>
2017-04-21 09:46:01 +0800

    Added 4 views

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # flaskr
+
+## 201721Ap+65
+Added Views for 
+1. /
+- Show all database entries
+2. /add
+- add title and text to the database
+3. /login
+- to authenticate a single user
+- hardcoded password
+4. /logout
+- use pop() to remove session from a dict (pros? cons?)
+
--- flaskr.py
+++ flaskr.py
@@ -44,3 +44,40 @@ def initdb_command():
     """Initialises the database"""
     init_db()
     print('Initialised the database.')
+
+@app.route('/')
+def show_entries():
+    db = get_db()
+    cur = db.execute('select title, text from entries order by id desc')
+    entries = cur.fetchall()
+    return render_template('show_emtries.html', entries=entries)
+
+@app.route('/add', methods=['POST'])
+def add_entry():
+    if not session.get('logged_in'):
+        abort(401)
+    db = get_db()
+    db.execute('insert into entries (title, text) values (?, ?)', [request.form['title'], request.form['text']])
+    db.commit()
+    flash('New entry was successfully posted')
+    return redirect(url_for('show_entries'))
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['usernmae'] != app.config['USERNAME']:
+            error = 'Invalid Username'
+        elseif request.form['password'] != app.config['PASSWORD']:
+            error = 'Invalid Passord'
+        else:
+            session['logged_in'] = True
+            flash('You are logged in')
+            return redirect(url_for('show_entries'))
+    return render_template('login.html', error=error)
+
+@app.route('/logout')
+def logout():
+    session.pop('logged_in', None)
+    flash('You were logged out')
+    return redirect(url_for('show_entries'))
